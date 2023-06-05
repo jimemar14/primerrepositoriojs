4ea9abaad138e9980a6be359d244f3c1806c5969
jimemar <jimenaornelamartinez@gmail.com>
2023-06-05 15:04:19 -0300

    Subir archivos al repositorio

---
+++ Mi proyecto/js/clase1.js
@@ -0,0 +1,22 @@
+/* palabrasReservada nombreSinEspacio = valor
+
+let
+const
+var (no usar)
+
+*/
+
+let camelCase;
+// let kebab-case; (no se usa tanto)
+let snake_case;
+
+// ; = no es necesario pero es un uso recomendable / cierra la declaración
+
+// let -- le puedo cambiar el valor
+let numerito = 5;
+console.log(numerito)
+numerito = 3;
+console.log(numerito)
+
+
+// const -- es constante
